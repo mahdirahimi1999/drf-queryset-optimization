02c9415df003391be3128057b73a8bb8b2614001
Mahdi Rahimi <mahdi.rahimi95@gmail.com>
2023-08-16 16:36:00 +0330

    Imrove get_queryset method

--- drf_queryset_optimization/mixins.py
+++ drf_queryset_optimization/mixins.py
@@ -3,9 +3,12 @@ class QuerysetOptimizationMixin:
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        if self.defer_fields:
-            queryset = queryset.defer(*self.defer_fields)
-        elif self.only_fields:
-            queryset = queryset.only(*self.only_fields)
+        defer_fields = getattr(self, 'defer_fields', [])
+        only_fields = getattr(self, 'only_fields', [])
+
+        if defer_fields:
+            queryset = queryset.defer(*defer_fields)
+        elif only_fields:
+            queryset = queryset.only(*only_fields)
 
         return queryset
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="drf-queryset-optimization",  # Change the package name here
-    version="0.1.0",
+    version="0.1.1",
     author="Mahdi Rahimi",
     author_email="mahdi.rahimi95@gmail.com",
     description="A package for optimizing querysets in Django REST Framework views and viewsets",
